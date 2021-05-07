8e34bdfbd91aee2e47b50d004fc38c6760832256
sooooner <tnsgh0101@gmail.com>
2021-05-07 19:25:50 +0900

    inference.py

--- inference.py
+++ inference.py
@@ -10,10 +10,14 @@ from utils.models.frcnn import Faster_RCNN
 from utils.utils import Anchor_Boxes
 
 ap = argparse.ArgumentParser()
-ap.add_argument("--path", required=True, type=str, help="Data path")
+ap.add_argument("--path", required=True, type=str, help="Image data path")
 args = ap.parse_args()
 
-def main(img):
+def main(img_path):
+    img = tf.io.read_file(img_path) 
+    img = tf.image.decode_jpeg(img, channels=3) 
+    img = tf.expand_dims(tf.image.resize(img, [432, 768])/255, 0)
+
     batch_size = 16
     scales = config.SCALES
     ratio = config.RATIO
@@ -36,9 +40,17 @@ def main(img):
         pool_size=pool_size
     )
 
-    frcnn.load_weights("./model_weight/frcnn")
+    frcnn.load_weights("./model_weight/step4").expect_partial()
 
-    cls, predict = frcnn(tf.expand_dims(img, 0))
+    # cls, predict = frcnn(img)
+    scores, rps, feature_map = frcnn.rpn(img)
+    rps = frcnn.rpn.inverse_bbox_regression(rps)
+    candidate_area, scores = frcnn.get_candidate((scores, rps, frcnn.n_test_pre_nms))
+    nms = frcnn.get_nms((candidate_area, scores, frcnn.n_test_post_nms))
+    rois = frcnn.roipool((feature_map, nms))
+    cls, bbox_reg, nms = frcnn.classifier((rois, nms))
+    predict = frcnn.classifier.inverse_bbox_regression(bbox_reg, nms)
+
     selected_indices = tf.image.non_max_suppression(predict[0], tf.squeeze(tf.cast(cls[0], tf.float32)), max_output_size=5, score_threshold=.5)
     anchors = tf.gather(predict[0], selected_indices)
     anchor = np.mean(anchors, axis=0)
@@ -54,6 +66,7 @@ def main(img):
     plt.show()
 
 if __name__ == '__main__':
+    # python inference.py --path=./res/train_imgs/001-1-1-01-Z17_A-0000001.jpg
     main(args.path)
 
     
