fa5690e14e7d876394d84ba2cefa59d5428e5fd6
sooooner <tnsgh0101@gmail.com>
2021-05-11 14:00:02 +0900

    inferece.py

--- inference.py
+++ inference.py
@@ -7,7 +7,7 @@ import config
 import cv2
 
 from utils.models.frcnn import Faster_RCNN
-from utils.utils import Anchor_Boxes
+from utils.utils import Anchor_Boxes, anchor_to_coordinate
 
 ap = argparse.ArgumentParser()
 ap.add_argument("--path", required=True, type=str, help="Image data path")
@@ -15,10 +15,10 @@ args = ap.parse_args()
 
 def main(img_path):
     img = tf.io.read_file(img_path) 
-    img = tf.image.decode_jpeg(img, channels=3) 
-    img = tf.expand_dims(tf.image.resize(img, [432, 768])/255, 0)
+    img_ = tf.image.decode_jpeg(img, channels=3)
+    img = tf.expand_dims(tf.image.resize(img_, [432, 768])/255, 0)
+    img_ = img_.numpy()
 
-    batch_size = 16
     scales = config.SCALES
     ratio = config.RATIO
     anchor_boxes = Anchor_Boxes(config.IMG_SIZE, scales, ratio)
@@ -42,28 +42,25 @@ def main(img_path):
 
     frcnn.load_weights("./model_weight/step4").expect_partial()
 
-    # cls, predict = frcnn(img)
-    scores, rps, feature_map = frcnn.rpn(img)
-    rps = frcnn.rpn.inverse_bbox_regression(rps)
-    candidate_area, scores = frcnn.get_candidate((scores, rps, frcnn.n_test_pre_nms))
-    nms = frcnn.get_nms((candidate_area, scores, frcnn.n_test_post_nms))
-    rois = frcnn.roipool((feature_map, nms))
-    cls, bbox_reg, nms = frcnn.classifier((rois, nms))
-    predict = frcnn.classifier.inverse_bbox_regression(bbox_reg, nms)
+    cls, predict = frcnn(img)
+    max_output_size = 5
 
-    selected_indices = tf.image.non_max_suppression(predict[0], tf.squeeze(tf.cast(cls[0], tf.float32)), max_output_size=5, score_threshold=.5)
-    anchors = tf.gather(predict[0], selected_indices)
-    anchor = np.mean(anchors, axis=0)
-    
-    x1 = int(anchor[0] - anchor[2]/2)
-    x2 = int(anchor[0] + anchor[2]/2)
-    y1 = int(anchor[1] - anchor[3]/2)
-    y2 = int(anchor[1] + anchor[3]/2)
-    cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), thickness=2)
-    fig, ax = plt.subplots(dpi=200)
-    ax.imshow(img)
-    ax.axis('off')
-    plt.show()
+    scores_order = tf.argsort(cls[0], direction='DESCENDING', axis=0)
+    boxes = tf.squeeze(tf.gather(predict[0], scores_order))
+    boxes = boxes[boxes[:, 2] > 16]
+    boxes = boxes[boxes[:, 3] > 16][:max_output_size]
+        
+    anchor = anchor_to_coordinate(np.mean(boxes, axis=0))
+    cv2.rectangle(
+        img_, 
+        (int(anchor[0]*2.5), int(anchor[2]*2.5)), (int(anchor[1]*2.5), int(anchor[3]*2.5)), 
+        (255, 0, 0), 
+        thickness=2
+    )
+
+    plt.imshow(img_)
+    plt.axis('off')
+    plt.savefig('fig1.png', dpi=200)
 
 if __name__ == '__main__':
     # python inference.py --path=./res/train_imgs/001-1-1-01-Z17_A-0000001.jpg
