bcee7f34f6baf01f10f7a9502b9d168131b0e631
sooooner <tnsgh0101@gmail.com>
2021-05-13 19:35:48 +0900

    db, server connect

--- inference.py
+++ inference.py
@@ -1,69 +1,58 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import json
+import requests
+import pymysql
 import pandas as pd
-import numpy as np
-import argparse
-import config
+import base64
 import cv2
+from utils.utils import anchor_to_coordinate
 
-from utils.models.frcnn import Faster_RCNN
-from utils.utils import Anchor_Boxes, anchor_to_coordinate
+def main():
+    conn = pymysql.connect(host='mysql', user='root', charset='utf8') 
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
 
-ap = argparse.ArgumentParser()
-ap.add_argument("--path", required=True, type=str, help="Image data path")
-args = ap.parse_args()
+    sql = "USE dacon;" 
+    cursor.execute(sql) 
 
-def main(img_path):
-    img = tf.io.read_file(img_path) 
-    img_ = tf.image.decode_jpeg(img, channels=3)
-    img = tf.expand_dims(tf.image.resize(img_, [432, 768])/255, 0)
-    img_ = img_.numpy()
+    sql = '''SELECT image_id, image FROM images'''
+    cursor.execute(sql) 
+    result = cursor.fetchall()
+    df = pd.json_normalize(result)
 
-    scales = config.SCALES
-    ratio = config.RATIO
-    anchor_boxes = Anchor_Boxes(config.IMG_SIZE, scales, ratio)
-    img_size = config.IMG_SIZE
-    anchor_boxes = anchor_boxes
-    k = config.K
-    n_sample = config.N_SAMPLE
-    backbone = config.BACKBONE
-    rpn_lambda = config.RPN_LAMBDA
-    pool_size = config.POOL_SIZE
+    img = base64.decodebytes(df.iloc[0]['image'])
+    img = tf.image.decode_jpeg(img, channels=3)
+    img = tf.expand_dims(tf.image.resize(img, [432, 768])/255, 0)
 
-    frcnn = Faster_RCNN(
-        img_size=img_size, 
-        anchor_boxes=anchor_boxes, 
-        k=k, 
-        n_sample=n_sample, 
-        backbone=backbone,
-        rpn_lambda=rpn_lambda, 
-        pool_size=pool_size
-    )
+    data = json.dumps({"signature_name": "serving_default", "instances": img.numpy().tolist()})
 
-    frcnn.load_weights("./model_weight/step4").expect_partial()
+    headers = {"content-type": "application/json"}
+    json_response = requests.post('http://host.docker.internal:8501/v1/models/frcnn:predict', data=data, headers=headers)
 
-    cls, predict = frcnn(img)
-    max_output_size = 5
+    predictions = json.loads(json_response.text)['predictions']
 
-    scores_order = tf.argsort(cls[0], direction='DESCENDING', axis=0)
-    boxes = tf.squeeze(tf.gather(predict[0], scores_order))
+    max_output_size = 3
+    img_ = img[0].numpy().copy()
+
+    scores_order = tf.argsort(predictions[0]['output_1'], direction='DESCENDING', axis=0)
+    boxes = tf.squeeze(tf.gather(predictions[0]['output_2'], scores_order))
     boxes = boxes[boxes[:, 2] > 16]
     boxes = boxes[boxes[:, 3] > 16][:max_output_size]
-        
-    anchor = anchor_to_coordinate(np.mean(boxes, axis=0))
+    boxes = tf.math.reduce_mean(boxes, axis=0)
+
+    anchor = anchor_to_coordinate(boxes.numpy())
     cv2.rectangle(
         img_, 
-        (int(anchor[0]*2.5), int(anchor[2]*2.5)), (int(anchor[1]*2.5), int(anchor[3]*2.5)), 
-        (255, 0, 0), 
-        thickness=2
+        (int(anchor[0]), int(anchor[2])), (int(anchor[1]), int(anchor[3])), 
+        (1, 0, 0), 
+        thickness=1
     )
 
     plt.imshow(img_)
     plt.axis('off')
-    plt.savefig('fig1.png', dpi=200)
+    plt.savefig('./test.png', dpi=200)
 
 if __name__ == '__main__':
-    # python inference.py --path=./res/train_imgs/001-1-1-01-Z17_A-0000001.jpg
-    main(args.path)
+    main()
 
     
